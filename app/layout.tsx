c91ef0167aa845e17b320d2eb32cfe50cde6916a
Tomas Di Mauro <tomas@tdm.ar>
2025-04-13 23:28:07 -0300

    [feat] improved start page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 
 import { Inter } from 'next/font/google';
+import { Toaster } from 'sonner';
 
 import './globals.css';
 
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en'>
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                {children}
+                <Toaster />
+            </body>
         </html>
     );
 }
